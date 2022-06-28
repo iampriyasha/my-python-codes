0ef3cf8fe5452b9f0a9103dcd83a18717312e2b6
iampriyasha <108354979+iampriyasha@users.noreply.github.com>
2022-06-28 12:00:07 -0400

    Delete Mycodes directory

--- Mycodes/first.py
+++
@@ -1,8 +0,0 @@
-inpA = int(input("enter an integer:"))
-inpB = int(input("enter another integer:"))
-input_D = input("do you want to add or subtract; press 0 for add, 1 for subtract")
-if input_D == "0":
-    inpC = inpA+inpB
-else:
-    inpC = inpA-inpB
-print (inpC)
